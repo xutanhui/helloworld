b4a04591391f3f6bc8464a48b19b07bd94dc26b7
xutanhui <32347374+xutanhui@users.noreply.github.com>
2018-05-01 19:25:02 +0800

    修改了一些东西
    这是一个测试

--- src/main/java/mavenhelloworld/helloworld/HelloWorld.java
+++ src/main/java/mavenhelloworld/helloworld/HelloWorld.java
@@ -3,6 +3,8 @@ package mavenhelloworld.helloworld;
 public class HelloWorld {
 	public static void main(String[] args) {
 		Person p = new Person();
+		System.out.println("说话开始");
 		System.out.println(p.sayHello());
+		System.out.println("说话结束");
 	}
 }
